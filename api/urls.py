f38a2ff71898fc3cc8f4972834489d0578de24a9
Ewerton12F <ewerton12freitas@gmail.com>
2022-04-17 09:25:46 -0300

    Layer responsible for defining API endpoints and handling application requests and responses

---
+++ api/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import *
+from . import views
+
+router = DefaultRouter()
+router.register('trainer', TrainerViewset, basename='trainer')
+#router.register('pokemon', PokemonViewset, basename='pokemon')
+urlpatterns = router.urls
+
+
+""" 
+urlpatterns = [
+    path('', include(c)),
+#    path('trainer/', TrainerViewset.as_view({
+#        'get': 'get_queryset',
+#        'post': 'create',
+#    })),
+#    path('trainer/authenticate/', views.trainerAuth),
+#    path('trainer/<int:pk>/', views.trainerId),
+#    path('trainer/<int:pk>/pokemon/', views.pokemonList),
+#    path('trainer/<int:pk>/pokemon/', views.pokemonCreate),
+#    path('trainer/<int:pk>/pokemon/<int:pk>/', views.pokemonDelete),
+]
+ """
