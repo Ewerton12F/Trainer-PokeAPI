11e678e01f4fd0d857965e84f111a9e4fd4112e1
Ewerton12F <ewerton12freitas@gmail.com>
2022-04-17 09:27:17 -0300

    Serializers allow complex data such as querysets and model instances to be converted to native Python datatypes that can then be easily rendered into JSON, XML or other content types.

---
+++ api/serializers.py
@@ -0,0 +1,72 @@
+from rest_framework import serializers
+from .models import *
+
+class PokemonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Pokemon
+        fields = [
+            'id', 
+            'name', 
+            'base_experience', 
+            'height', 
+            'is_default', 
+            'order', 
+            'weight',
+            'trainer_id'
+        ]
+
+class TrainerSerializer(serializers.ModelSerializer):
+    pokemons = PokemonSerializer(many=True)
+
+    class Meta:
+        model = Trainer
+        fields = [
+            'id',
+            'nickname', 
+            'first_name', 
+            'last_name', 
+            'email', 
+            'password',
+            'team', 
+            'pokemons_owned',
+            'pokemons',
+        ]
+        extra_kwargs = {
+            'id': {'read_only': True},
+            'email': {'write_only': True},
+            'password': {'write_only': True},
+            'pokemons_owned': {'read_only': True},
+        }
+
+    def create(self, validated_data):
+        pokemons = validated_data.pop('pokemons')
+        trainer = Trainer.objects.create(**validated_data)
+        for pokemon in pokemons:
+            Pokemon.objects.create(**pokemon, trainer=trainer)
+        return trainer
+    """ 
+    def update(self, instance, validated_data):
+        choices = validated_data.pop('choices')
+        instance.title = validated_data.get("title", instance.title)
+        instance.save()
+        keep_choices = []
+        for choice in choices:
+            if "id" in choice.keys():
+                if Choice.objects.filter(id=choice["id"]).exists():
+                    c = Choice.objects.get(id=choice["id"])
+                    c.text = choice.get('text', c.text)
+                    c.save()
+                    keep_choices.append(c.id)
+                else:
+                    continue
+            else:
+                c = Choice.objects.create(**choice, question=instance)
+                keep_choices.append(c.id)
+
+        for choice in instance.choices:
+            if choice.id not in keep_choices:
+                choice.delete()
+
+        return instance
+     """
